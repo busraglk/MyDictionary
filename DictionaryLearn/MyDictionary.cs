1d47e5733f0650c945afaac2108e616703240308
busraglk <busragelik@gmail.com>
2021-01-23 16:11:23 +0300

    Add project files.

---
+++ DictionaryLearn/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryLearn
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _key;
+        TValue[] _value;
+
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] _tempKeyArray = _key;
+            TValue[] _tempValueArray = _value;
+
+            _key = new TKey[_key.Length + 1];
+            _value = new TValue[_value.Length + 1];
+
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+            {
+                _key[i] = _tempKeyArray[i];
+                _value[i] = _tempValueArray[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+    }
+}
---
+++ DictionaryLearn/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dictionary<TKey,TValue>
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Büşra");
+            myDictionary.Add(2, "Barış");
+            myDictionary.Add(3, "Ahsen");
+            myDictionary.Add(4, "Behlül");
+            myDictionary.Add(5, "Özge");
+            Console.WriteLine(myDictionary.Count);
+
+            MyDictionary<int, string> myDictionary2 = new MyDictionary<int, string>();
+            myDictionary2.Add(10, "Gök");
+            myDictionary2.Add(11, "Önçal");
+            myDictionary2.Add(12, "Kuru");
+            Console.WriteLine(myDictionary2.Count);
+
+
+            Console.ReadLine();
+        }
+    }
+}
